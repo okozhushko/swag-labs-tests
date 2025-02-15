c3c9738432bfcfc6abf6de91c8c6f56cfbce900f
Oleh Kozhushko <oleg.k.linkedin@gmail.com>
2025-02-15 15:05:46 +0100

    Updated test

--- src/test/java/org/example/base/tests/TestCheckProductsPage.java
+++ src/test/java/org/example/base/tests/TestCheckProductsPage.java
@@ -18,6 +18,7 @@ public class TestCheckProductsPage {
             " fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.";
     String secondItemName = "Sauce Labs Fleece Jacket";
     String secondItemPrice = "$49.99";
+    String addToCartText = "Add to cart";
 
     @Test
     public void testLoginWithValidCredentials() {
@@ -28,19 +29,19 @@ public class TestCheckProductsPage {
         AllItemsPage.initAllItemsPage()
                 .checkAllPageItemsAvailable()
                 .checkItemDetails(3, expectedProductFirst)
-                .clickAddItemBtn(3, "Add to cart")
+                .clickAddItemBtn(3, addToCartText)
                 .checkItemAddedToBucket("1")
                 .checkRemoveItemBtn(3)
                 .clickOnBucketIcon()
-                .checkCartItem(1,expectedProductFirst)
+                .checkCartItem(1, expectedProductFirst)
                 .checkCartItemDescription(1)
                 .clickContinueShopingBtn()
                 .checkItemDetails(4, expectedProductSecond)
-                .clickAddItemBtn(4, "Add to cart")
+                .clickAddItemBtn(4, addToCartText)
                 .checkItemAddedToBucket("2")
                 .checkRemoveItemBtn(3)
                 .clickOnBucketIcon()
-                .checkCartItem(2,expectedProductSecond);
+                .checkCartItem(2, expectedProductSecond);
 
         //.removeCartItem()
 
